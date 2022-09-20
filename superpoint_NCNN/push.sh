57dc6fa6f0c515f1dc386ce2a07b995917855ffb
Richard <luziwen@cvte.com>
2022-09-20 17:35:42 +0800

    superpoint NCNN done, now working on superpoint SNPE

---
+++ superpoint_NCNN/push.sh
@@ -0,0 +1,2 @@
+adb push /home/cvte-vm/Deep_Feature_Extract/pytorch-superpoint/implementation/superpoint_NCNN/build/android/bin/NCNN_superpoint /data/local/tmp/superpoint/bin
+adb push /home/cvte-vm/Deep_Feature_Extract/pytorch-superpoint/implementation/superpoint_NCNN/build/android/library/libsp_lib.so /data/local/tmp/superpoint/lib
